87d42f4b73d4f98e9d654d4d65ecca02d5d42c5f
Jan Böhmer <mail@jan-boehmer.de>
2024-01-08 21:58:39 +0100

    Use phpstan conditional return for SettingsManager as a better solution

--- src/Manager/SettingsManagerInterface.php
+++ src/Manager/SettingsManagerInterface.php
@@ -36,8 +36,8 @@ interface SettingsManagerInterface
      * @param  string  $settingsClass The settings class to get the instance for. This can either be a class string, or the short name of the settings class.
      * @return object
      * @template T of object
-     * @phpstan-param class-string<T> $settingsClass
-     * @phpstan-return T
+     * @phpstan-param string|class-string<T> $settingsClass
+     * @phpstan-return ($settingsClass is class-string<T> ? T : object)
      */
     public function get(string $settingsClass): object;
 

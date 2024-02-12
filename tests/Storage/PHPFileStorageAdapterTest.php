876916cbd3f9776907d67c1c9dd987c10021adc4
Jan Böhmer <mail@jan-boehmer.de>
2024-02-12 22:35:32 +0100

    Added test for PHPFileStorageAdapter and fixed test config storage directory

--- tests/Storage/PHPFileStorageAdapterTest.php
+++ tests/Storage/PHPFileStorageAdapterTest.php
@@ -26,21 +26,22 @@
 namespace Jbtronics\SettingsBundle\Tests\Storage;
 
 use Jbtronics\SettingsBundle\Storage\JSONFileStorageAdapter;
+use Jbtronics\SettingsBundle\Storage\PHPFileStorageAdapter;
 use PHPUnit\Framework\TestCase;
 
-class JSONFileStorageAdapterTest extends TestCase
+class PHPFileStorageAdapterTest extends TestCase
 {
     use FileAdapterTestTrait;
 
     private const STORAGE_DIR = __DIR__.'/../TestApplication/var/settings_test';
 
-    private JSONFileStorageAdapter $service;
+    private PHPFileStorageAdapter $service;
 
     protected function setUp(): void
     {
         //Remove the storage directory for a clean test
         $this->cleanStorageDir(self::STORAGE_DIR);
-        $this->service = new JSONFileStorageAdapter(self::STORAGE_DIR, 'settings.json');
+        $this->service = new PHPFileStorageAdapter(self::STORAGE_DIR, 'settings.php');
     }
 
     public function testSaveAndLoad(): void
@@ -48,34 +49,34 @@ class JSONFileStorageAdapterTest extends TestCase
         $this->service->save('test', ['test' => 'value']);
 
         //Afterward a file should exist
-        $this->assertFileExists(self::STORAGE_DIR.'/settings.json');
-        $this->assertEquals(['test' => ['test' => 'value']], json_decode(file_get_contents(self::STORAGE_DIR.'/settings.json'), true));
+        $this->assertFileExists(self::STORAGE_DIR.'/settings.php');
+        $this->assertEquals(['test' => ['test' => 'value']], require self::STORAGE_DIR.'/settings.php');
 
         //We should be able to save files with different filenames
-        $this->service->save('other_file', ['test' => 'value'], ['filename' => 'test.json']);
-        $this->assertFileExists(self::STORAGE_DIR.'/test.json');
+        $this->service->save('other_file', ['test' => 'value'], ['filename' => 'test.php']);
+        $this->assertFileExists(self::STORAGE_DIR.'/test.php');
 
         //When we save more data, then it should be added to the existing file
         $this->service->save('test2', ['test' => 'value', 'test2' => 'value2']);
         $this->assertEquals([
             'test' => ['test' => 'value'],
             'test2' => ['test' => 'value', 'test2' => 'value2']
-        ], json_decode(file_get_contents(self::STORAGE_DIR.'/settings.json'), true, 512, JSON_THROW_ON_ERROR));
+        ], require self::STORAGE_DIR.'/settings.php');
 
         //We should be able to load the data again
         $this->assertEquals(['test' => 'value'], $this->service->load('test'));
         $this->assertEquals(['test' => 'value', 'test2' => 'value2'], $this->service->load('test2'));
 
         //We should be able to load the data again with a different filename
-        $this->assertEquals(['test' => 'value'], $this->service->load('other_file', ['filename' => 'test.json']));
+        $this->assertEquals(['test' => 'value'], $this->service->load('other_file', ['filename' => 'test.php']));
 
         //This should also work fine with the always_reload_file option set
-        $this->assertEquals(['test' => 'value'], $this->service->load('other_file', ['filename' => 'test.json' ,'always_reload_file' => true]));
+        $this->assertEquals(['test' => 'value'], $this->service->load('other_file', ['filename' => 'test.php' ,'always_reload_file' => true]));
 
         //If we request a key that does not exist, then it should return null
         $this->assertNull($this->service->load('does_not_exist'));
 
         //Or if we request a file that does not exist
-        $this->assertNull($this->service->load('does_not_exist', ['filename' => 'does_not_exist.json']));
+        $this->assertNull($this->service->load('does_not_exist', ['filename' => 'does_not_exist.php']));
     }
 }
